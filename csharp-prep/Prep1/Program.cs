f4b8c1e205ad26286e79eb4b577d0e9357defe14
Gavin1802 <pet20039@byui.edu>
2024-01-13 16:21:56 -0700

    For the assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string myName = "Peterson";
+
+        Console.WriteLine("A Name: " + myName);
+
+
+        //readLine
+        System.Console.WriteLine("What's your age?");
+        var ageString = Console.ReadLine();
+
+        //Converting Variables
+        var age = int.Parse(ageString);
+
+        //Conditionals
+        if (age < 18) { //curly braces creates blocks
+            System.Console.WriteLine("You're a minor");
+            System.Console.WriteLine("And you're cringe");
+        }
+        else{
+            System.Console.WriteLine("You're Old");
+        }
+        
+        }
     }
 }
