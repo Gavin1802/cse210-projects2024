20f7034aee16c3e1483151b821f7b8261ce1fd93
Gavin1802 <pet20039@byui.edu>
2024-01-14 11:12:07 -0700

    Did the stretch challenge

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -13,29 +13,44 @@ class Program
         string letter;
         var congrats = "You passed! Great job!";
         var encourage = "You didn't pass but you'll do better next time!";
+        string symbol = "";
+
+        //If/Else for their symbol
+        if ((number>=90&&number<=94)||(number>=80&&number<=84)||(number>=70&&number<=74)||(number>=60&&number<=64))
+        {
+            symbol = "-";
+        }
+        else if ((number>=87&&number<=89)||(number>=77&&number<=79)||(number>=67&&number<=69))
+        {
+            symbol = "+";
+        }
+        else
+        {
+            symbol = "";
+        }
 
         //If/Else for their letter grade
         if (number >= 90)
         {
-            letter = "A";
+            letter = "A"+symbol;
             // Console.WriteLine(grd + "A");
             // Console.WriteLine(congrats);
         } 
         else if (number >= 80 && number < 90)
         {
-            letter = "B";
+            letter = "B"+symbol;
             // Console.WriteLine(grd + "B");
             // Console.WriteLine(congrats);
         }
         else if (number >= 70 && number < 80)
         {
-            letter = "C";
+            letter = "C"+symbol;
             // Console.WriteLine(grd + "C");
             // Console.WriteLine(congrats);
         }
         else if (number >= 60 && number < 70)
         {
-            letter = "D";
+            letter = "D"+symbol;
             // Console.WriteLine(grd + "D");
             // Console.WriteLine(encourage);
         }
@@ -56,7 +71,5 @@ class Program
         {
             Console.WriteLine(encourage);
         }
-
-
     }
 }
