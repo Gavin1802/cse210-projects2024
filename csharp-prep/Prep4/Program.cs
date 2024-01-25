fe163dd5bb75ae8b629bfae2a0356c804d768997
Gavin1802 <pet20039@byui.edu>
2024-01-24 19:06:33 -0700

    Did the HW finally

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,25 +1,37 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Prep4 World!");
-        // lists are only one type of thing, like all ints, or all floats
+        // lists are of only one type, like all ints, or all floats
 
         List<int> myInts = new List<int>();
-        var moreInt = new List<int>();
+        int num = 1;
+        System.Console.WriteLine("Enter a list of numbers to add to a list, type 0 when finished.");
 
-        moreInt.Add(34);
-        moreInt.Add(10);
-
-
-        foreach(var n in moreInt) {
-            System.Console.WriteLine($"n = {n}");
+        while(num != 0)
+        {
+            //prompt the user for numbers for myInts and add their number to the list
+            System.Console.Write("Enter a Number: ");
+            num = int.Parse(Console.ReadLine());
+            myInts.Add(num);
         }
 
-        
+        if(num == 0){
+            //Sum up and print the ints in the list
+            int sum = myInts.Sum();
+            Console.WriteLine($"The sum is {sum}.");
 
+            //Calculate and print the average
+            var average = myInts.Count > 0 ? myInts.Average() : 0;
+            System.Console.WriteLine($"The average is {average}.");
 
+            //Display the largest number
+            int largest = myInts.Max();
+            System.Console.WriteLine($"The largest number is: {largest}");
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -21,21 +21,46 @@ class Program
 
         int more = Add2(10);
 
-        void PrintName(string name) {
-            if (name.Length == 0){
-                return;
-            }
-            System.Console.WriteLine($"Name is {name}");
+        static void DisplayWelcome() {
+            System.Console.WriteLine($"Welcome to the Program!");
         }
 
-        PrintName("Bob");
+        static string PromptUserName() {
+            System.Console.Write($"Please enter your name: ");
+            string userName = System.Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber() {
+            System.Console.Write($"Please enter your favorite number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+        }
+
+        static int SquareNumber(int PromptUserNumber) {
+            return PromptUserNumber * PromptUserNumber;
+        }
+
+        static void DisplayResult(string PromptUserName, int SquareNumber){
+            System.Console.WriteLine($"{PromptUserName}, the square of your number is {SquareNumber}"); 
+        }
+       
+       
+        // void PrintName(string name) {
+        //     if (name.Length == 0){
+        //         return;
+        //     }
+        //     System.Console.WriteLine($"Name is {name}");
+        // }
+
+        // PrintName("Bob");
 
         // functions should always start with capital letters!!
 
         //variabe scope
-        var y = 0;
-        { 
-            var w = 10;
-        }
+        // var y = 0;
+        // { 
+        //     var w = 10;
+        // }
     }
 }
