733ad93c4f038dd84b7a83d7f4485842d0b496d8
Gavin1802 <pet20039@byui.edu>
2024-02-02 20:33:32 -0700

    Finishing Up Stuff

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
+
+namespace MySpace{
+    class Program
     {
-        Console.WriteLine("Hello Learning02 World!");
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello Learning02 World!");
+
+            var cars = new List<Car>();
+
+            var owner = new Owner();
+                owner.name = "James";
+                owner.phone = "555-5555";
+            
+            var car = new Car("Honda", "Civic", 10, 30, owner);
+            
+            cars.Add(car);
+
+            owner = new Owner();
+            owner.name = "Susan";
+            owner.phone = "999-9959";
+
+            car = new Car("Ford", "F-150", 30, 5, owner);
+            
+            cars.Add(car);
+
+            foreach (var c in cars) 
+            {
+                c.Display();
+                int range = c.TotalRange();
+            }
+        }
     }
 }
