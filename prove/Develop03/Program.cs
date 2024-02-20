aee5dd3ef4eeaedf1383a8eebe8342e646466b46
Gavin1802 <pet20039@byui.edu>
2024-02-19 22:09:06 -0700

    Updated a comment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -10,7 +10,7 @@ class Program
         var scriptureText = "Behold, I am Jesus Christ, whom the prophets testified shall come into the world. And behold, I am the light and the life of the world; and I have drunk out of that bitter cup which the Father hath given me, and have glorified the Father in taking upon me the sins of the world, in the which I have suffered the will of the Father in all things from the beginning.";
         Scripture scripture = new Scripture(reference, scriptureText);
 
-        //Clear console screen and display the scripture
+        //Call the method to display
         ClearAndDisplayScripture(scripture);
 
         // Prompt the user to press enter to hide 3 words or quit
